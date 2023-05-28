a5020e72bdd9b3c47993c59aa5ccbe1c7bd68d1c
Aaron M. Carr <90634807+amcarr-ds@users.noreply.github.com>
2023-05-28 12:34:05 -0700

    Upload notebooks with beginning code for scaping news sites using NewAPI

---
+++ deliverables/01tables_setup.sql
@@ -0,0 +1,29 @@
+USE 509_final_proj;
+
+DROP TABLE IF EXISTS nar_temp;
+DROP TABLE IF EXISTS news_articles;
+
+/**/
+CREATE TABLE news_articles
+(
+text_id SMALLINT UNSIGNED AUTO_INCREMENT,
+source_name VARCHAR(1000),
+author VARCHAR(1000),
+title VARCHAR(1000),
+url VARCHAR(1000),
+publish_date VARCHAR(30),
+article_text LONGTEXT,
+CONSTRAINT pk_text_id PRIMARY KEY (text_id)
+);
+DESC news_articles;
+
+/**/
+CREATE TABLE nar_temp
+(
+source_name VARCHAR(1000),
+author VARCHAR(1000),
+title VARCHAR(1000),
+url VARCHAR(1000),
+publish_date VARCHAR(30)
+);
+DESC nar_temp;
