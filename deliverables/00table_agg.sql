a6be0081079494d2017acdda99d77b6e7cc5c9cb
Aaron M. Carr <90634807+amcarr-ds@users.noreply.github.com>
2023-06-08 14:15:12 -0700

    Update data CSV

--- deliverables/00table_agg.sql
+++ deliverables/00table_agg.sql
@@ -14,7 +14,7 @@ SELECT DISTINCT source_name, title, article_text
 FROM news_articles
 WHERE article_text IS NOT NULL
 	AND (source_name="CNN" OR source_name="The Washington Post" OR source_name="Fox News" OR source_name="Breitbart News")
-LIMIT 2000;
+LIMIT 10000;
 
 SELECT
 source_name,
@@ -31,3 +31,13 @@ WHERE article_text IS NULL
 LIMIT 10000;
 
 DESC news_articles;
+
+SELECT
+source_name,
+CAST(LEFT(publish_date, 10) AS DATE) AS date,
+COUNT(*) AS source_count,
+COUNT(*) / sum(count(*)) over () AS source_dist
+FROM news_articles
+WHERE (source_name="CNN" OR source_name="Fox News" OR source_name="Breitbart News")
+GROUP BY source_name, CAST(LEFT(publish_date, 10) AS DATE)
+ORDER BY CAST(LEFT(publish_date, 10) AS DATE), source_name;
