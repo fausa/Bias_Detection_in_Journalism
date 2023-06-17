897135f00d2e758d05d69ad93707fabffbc2ed30
Aaron M. Carr <90634807+amcarr-ds@users.noreply.github.com>
2023-06-17 11:35:35 -0700

    Modeling updates

--- deliverables/00table_agg.sql
+++ deliverables/00table_agg.sql
@@ -39,6 +39,7 @@ CAST(LEFT(publish_date, 10) AS DATE) AS date,
 COUNT(*) AS source_count,
 COUNT(*) / sum(count(*)) over () AS source_dist
 FROM news_articles
-WHERE (source_name="CNN" OR source_name="Fox News" OR source_name="Breitbart News")
-GROUP BY source_name, CAST(LEFT(publish_date, 10) AS DATE)
+WHERE (source_name="CNN" OR source_name="The Washington Post"  OR source_name="Fox News" OR source_name="Breitbart News")
+	AND CAST(LEFT(publish_date, 10) AS DATE) > '2023-06-03'
+GROUP BY source_name, CAST(LEFT(publish_date, 10) AS DATE) with rollup
 ORDER BY CAST(LEFT(publish_date, 10) AS DATE), source_name;
